8e66d0fc90408626d3f42acc0f5b661bc402d9c5
Yuri Mendonça Silva <Yurimendonca25@gmail.com>
2026-05-30 14:58:38 -0300

    javascript add

---
+++ script.js
@@ -0,0 +1,11 @@
+function encomendar(Croissant) {
+    alert("Ótima escolha! Você selecionou: " + Croissant + "! Entre em contato via WhatsApp.");
+}
+
+function encomendar(Cinnamon) {
+    alert("Ótima escolha! Você selecionou: " + Cinnamon + "! Entre em contato via WhatsApp.");
+}
+
+function encomendar(Sourdough) {
+    alert("Ótima escolha! Você selecionou: " + Sourdough + "! Entre em contato via WhatsApp.");
+}
